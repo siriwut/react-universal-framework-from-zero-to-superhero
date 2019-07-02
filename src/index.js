a90c9100bf0c9a7977cb51cf8e8bc4b57999c42a
Tape <siriwut.pw@gmail.com>
2019-07-02 23:07:01 +0700

    wip: begin setup babel

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import http from 'http'
+
+app = express()
+
+const server = http.createServer(app)
+
+app.get('*', function(req, res) {
+  res.send('Hello World!')
+})
+
+server.listen(process.env.PORT || 3000, (error) => {
+  if (error) {
+    console.log(error)
+  }
+
+  console.log('🚀 started')
+})
