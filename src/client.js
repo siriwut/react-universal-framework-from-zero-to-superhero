68c0aedde0b6ed8571025040a6f412c233fbe4fe
Tape <siriwut.pw@gmail.com>
2019-07-10 23:58:41 +0700

    add ssr semantic ui

--- src/client.js
+++ src/client.js
@@ -1,5 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect } from 'react'
+import { hydrate } from 'react-dom'
+import {
+  ThemeProvider,
+  StylesProvider,
+} from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import theme from './theme'
+
+function Main() {
+  // useEffect(() => {
+  //   const jssStyles = document.querySelector(
+  //     '#jss-server-side',
+  //   )
+  //   if (jssStyles) {
+  //     jssStyles.parentNode.removeChild(jssStyles)
+  //   }
+  // }, [])
+
+  return (
+    <StylesProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </StylesProvider>
+  )
+}
+
+hydrate(<Main />, document.getElementById('app'))
--- src/server.js
+++ src/server.js
@@ -4,10 +4,38 @@ import path from 'path'
 import { renderToString } from 'react-dom/server'
 import { compile } from 'handlebars'
 
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {
+  ServerStyleSheets,
+  ThemeProvider,
+  StylesProvider,
+} from '@material-ui/styles'
+
+import theme from './theme'
+
 import App from './App'
 import template from './template.handlebars'
 
 const app = express()
+
+function handleRenderHtml(req, res) {
+  const materialSheets = new ServerStyleSheets()
+  const html = renderToString(
+    materialSheets.collect(
+      <StylesProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </StylesProvider>,
+    ),
+  )
+  const materialCss = materialSheets.toString()
+
+  res.send(
+    template({ body: html, materialCss: materialCss }),
+  )
+}
 
 app.use(
   express.static(
@@ -15,10 +43,6 @@ app.use(
   ),
 )
 
-app.get('/', function(req, res) {
-  const html = renderToString(<App />)
-
-  res.send(template({ body: html }))
-})
+app.use(handleRenderHtml)
 
 export default app
